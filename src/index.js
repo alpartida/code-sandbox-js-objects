7e8dc5e6793c34385203e211bbde888e583bcc49
Alex Partida <60553154+alpartida@users.noreply.github.com>
2020-08-23 16:53:41 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+let weather1 = {};
+console.log(weather1);
+
+let weather = {
+  temp: 18,
+  humidity: 15,
+  windspeed: 10
+};
+console.log(weather.temp);
+console.log(weather.humidity);
+console.log(weather.windspeed);
+
+console.log(weather);
+
+let forecast = [
+  {
+    day: "Monday",
+    temp: 60
+  },
+  {
+    day: "Tuesday",
+    temp: 50
+  },
+  {
+    day: "Wednesday",
+    temp: 55
+  },
+  {
+    day: "Thursday",
+    temp: 57
+  },
+  {
+    day: "Friday",
+    temp: 58
+  },
+  {
+    day: "Saturday",
+    temp: 59
+  },
+  {
+    day: "Sunday",
+    temp: 60
+  }
+];
+console.log(forecast);
